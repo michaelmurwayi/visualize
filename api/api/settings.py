dcc5070aa7219834c5bb845a45b3c7bb25e3146e
Michael Murwayi <mikemurwayi7@gmail.com>
2024-04-30 02:18:17 -0400

    ft-backend: Adding mysql database configurations

--- api/api/settings.py
+++ api/api/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'canvas',
+        'USER': 'root',
+        'PASSWORD': 'C11h28no3',
+        'HOST': 'localhost',  # Or your MySQL host
+        'PORT': '3306',       # Or your MySQL port
     }
 }
 
