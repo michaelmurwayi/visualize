6e8f366bce3a87f949da8443fb51ea172117ae66
Michael Murwayi <mikemurwayi7@gmail.com>
2024-05-05 21:49:12 -0400

    ft-auth: Adding user registration route to api

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import UserCreate
+
 
 urlpatterns = [
     path('token/', obtain_auth_token, name='api_token_auth'),
+    path('register/', UserCreate.as_view(), name='user_create'),
+
 ]
--- api/app/views.py
+++ api/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+
+class UserCreate(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
