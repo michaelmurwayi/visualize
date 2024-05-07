fbe7f64bb9c1f561aeb31c90e7bc717eae9e90e8
Michael Murwayi <mikemurwayi7@gmail.com>
2024-05-07 06:50:09 -0400

    ft-file-structure: Layout for structure of components and features

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+// import HomePage  from "./components/HomePage";
+import Site from "./components/Site/site"
+
+
+class App extends React.Component{
+    render(){
+      return(
+        <div>
+          <BrowserRouter>
+            <Routes>
+              {/* <Route exact path='/' Component={Home}/> */}
+              <Route exact path='site' Component={Site}/>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      )
+    }
+  }
+
+  
 export default App;
---
+++ dashboard/src/components/Site/site.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Site = () =>{
+    return (
+        <div>
+            <p>We are here</p>
+        </div>
+    );
+}
+
+export default Site 
